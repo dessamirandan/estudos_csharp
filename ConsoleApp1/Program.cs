aabca7a66309f7b7cd71eecb9f579ad8e67f8ce4
Andressa Miranda <amirandamachado@gmail.com>
2025-05-31 16:34:31 -0300

    aula 5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,8 +12,13 @@ namespace PrimeiroProjeto //Nome da aplicão
       double altura = 1.71433;
       int idade = 28;
       Console.WriteLine("Olá, " + name + ". Você possui " + idade + " anos, de sexo " + sexo + ", e uma altura de " + altura); //Exibe um texto + variável
-      Console.ReadLine();
 
+      double x = 10.1234566;
+      Console.WriteLine(); //exibe m uma nova linha
+      Console.WriteLine(x); //todos numeros apos o ponto
+      Console.WriteLine(x.ToString("F2")); //duas casas
+      Console.WriteLine(x.ToString("F4")); //quatro casas
+      Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));//imprime com o separador ponto
     }
   }
 }
