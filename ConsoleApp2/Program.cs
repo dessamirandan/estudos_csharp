9855c8acaecbfcf7e954f9ffc727ce995c0d9043
Andressa Miranda <amirandamachado@gmail.com>
2025-05-31 16:57:15 -0300

    aula 6

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SegundoProjeto
+{
+  internal class Programa
+  {
+    static void Main(string[] args)
+    {
+      int a, b, resultado;
+      a = 12;
+      b = 4;
+      resultado = a + b;
+      Console.WriteLine("O resultado da soma é: " + resultado);
+
+      float c, d, total;
+      c = 1.2f; //indicar para o compilador o tipo da varíavel
+      d = 2.8f; //boas práticas de programacão
+      total = c + d;
+      Console.WriteLine("O resultado da soma é igual a " + total);
+    }
+  }
+}
--- ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c78f3d25bd995a2f299244837ae516c5d0bf6d")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
