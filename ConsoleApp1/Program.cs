e34b2c2620a34397409831a07880eef5d17f0fbe
Andressa Miranda <amirandamachado@gmail.com>
2025-05-31 15:40:24 -0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; //
+
+namespace PrimeiroProjeto //Nome da aplicão
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      string name = "Andressa"; // tipo nome = valor;
+      char sexo = 'f';
+      Console.WriteLine("Hello " + name + ", sexo " + sexo); //Exibe um texto + variável
+
+    }
+  }
+}
