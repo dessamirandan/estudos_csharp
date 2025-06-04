d8024949fa2364c4e3072c4755778874a43d6daa
Andressa Miranda <amirandamachado@gmail.com>
2025-06-04 11:15:51 -0300

    Resolucao URI 1009

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+114c9e6fff3f4db3647cfee2bddf5d6db459b57d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d47ee30f6d91d04424a7cfc15b59e32fbf57d22a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
