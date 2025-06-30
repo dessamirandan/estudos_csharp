c22532e6dd51f702689783b62123a30567e253e9
Andressa Miranda <amirandamachado@gmail.com>
2025-06-30 20:26:45 -0300

    atualizacoes de sistema

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964689cc0a4a2595af354b420246217cb6a55009")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b629dcb2980129461e008e19120a4704b17fad6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ URI1038/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Xml.Serialization;
+
+namespace URI1038
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      int codigo, quantidade;
+      double total, pagar;
+      String[] pedido = Console.ReadLine().Split(' ');
+      codigo = int.Parse(pedido[0], CultureInfo.InvariantCulture);
+      quantidade = int.Parse(pedido[1], CultureInfo.InvariantCulture);
+      if (codigo == 1)
+      {
+        total = 4.00;
+      }
+      else if (codigo == 2)
+      {
+        total = 4.50;
+      }
+      else if (codigo == 3)
+      {
+        total = 5.00;
+      }
+      else if (codigo == 4)
+      {
+        total = 2.00;
+      }
+      else
+      {
+        total = 1.50;
+      }
+      pagar = total * quantidade;
+
+      Console.WriteLine("Total: R$ " + pagar.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+  }
+}
