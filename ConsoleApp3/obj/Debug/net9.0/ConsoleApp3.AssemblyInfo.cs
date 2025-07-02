1fe1d369d2ce4a3871cd00418c819041f3b27f9c
Andressa Miranda <amirandamachado@gmail.com>
2025-07-02 19:56:09 -0300

    Ignorar arquivos

--- ConsoleApp3/obj/Debug/net9.0/ConsoleApp3.AssemblyInfo.cs
+++ ConsoleApp3/obj/Debug/net9.0/ConsoleApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b629dcb2980129461e008e19120a4704b17fad6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c22532e6dd51f702689783b62123a30567e253e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
