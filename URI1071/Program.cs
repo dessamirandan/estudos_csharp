b1ae6738d0179cd255b4f8bce97ff9d721957e39
Andressa Miranda <amirandamachado@gmail.com>
2025-07-02 21:24:10 -0300

    Resolucao de exercicios uri1071

---
+++ URI1071/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace URI1071
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      int X, Y, SOMA;
+      X = int.Parse(Console.ReadLine());
+      Y = int.Parse(Console.ReadLine());
+      SOMA = 0;
+
+      for (int i = Math.Min(X, Y) + 1; i < Math.Max(X, Y); i++)
+      {
+        if (i % 2 != 0)
+        {
+          SOMA = SOMA + i;
+        }
+      }
+        Console.WriteLine(SOMA);
+    }
+  }
+}
+
